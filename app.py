455f29e96216340f782e7095ebc3d3d5586b2334
ANAND\Anand <anandandhs@gmail.com>
2023-06-21 04:28:01 +0530

    sample get endpoint

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+@app.route('/random_numbers')
+def random_numbers():
+    numbers = [random.randint(1, 10) for _ in range(10)]
+    return ', '.join(str(num) for num in numbers)
 
 if __name__ == '__main__':
     app.run()
